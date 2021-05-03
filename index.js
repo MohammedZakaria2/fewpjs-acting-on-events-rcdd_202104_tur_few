706ec0951e2835f745028a07c5122811ea7fbaa3
Mohammed Zakaria Alalaya <mohammed.alalaya@gmail.com>
2021-05-03 01:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code here
 function moveDodgerLeft(){
-     const dodger = document.quarySelector("");
+     const dodger = document.quarySelector("#dodger");
       dodger.style.bottom = "100px";
 }
